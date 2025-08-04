31168938d78e3277314227a1420c155d126beacc
m1xxos <66390094+m1xxos@users.noreply.github.com>
2025-08-04 20:59:06 +0300

    feat:arr stack

---
+++ terraform/arr.tf
@@ -0,0 +1,10 @@
+resource "portainer_stack" "arr-stack" {
+  name                      = "arr-stack"
+  method                    = "repository"
+  deployment_type           = "standalone"
+  endpoint_id               = var.endpoint_id
+  repository_url            = var.repository_url
+  repository_reference_name = var.repository_reference_name
+  file_path_in_repository   = "stacks/arr-stack/compose.yaml"
+  filesystem_path           = var.filesystem_path
+}
