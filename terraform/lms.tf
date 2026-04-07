8dc32105c1b80352cd5c9d03f118a8614975ced9
m1xxos <66390094+m1xxos@users.noreply.github.com>
2026-04-07 16:43:44 +0300

    feat:lms service

---
+++ terraform/lms.tf
@@ -0,0 +1,23 @@
+resource "random_password" "lms_app" {
+  length = 20
+}
+
+resource "portainer_stack" "lms" {
+  name                      = "lms"
+  method                    = "repository"
+  deployment_type           = "standalone"
+  endpoint_id               = var.endpoint_id
+  repository_url            = var.repository_url
+  repository_reference_name = var.repository_reference_name
+  file_path_in_repository   = "stacks/lms/compose.yaml"
+  filesystem_path           = var.filesystem_path
+  stack_webhook             = true
+  update_interval           = var.update_interval
+  pull_image                = true
+  force_update              = true
+
+  env {
+    name = "APP_SECRET"
+    value = random_password.lms_app.result
+  }
+}
